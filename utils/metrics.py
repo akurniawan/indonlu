13db97dafefe17bfefd2b4322df087fb69979226
akurniawan <akurniawan.cs@gmail.com>
2021-01-28 10:36:10 +0100

    add logging and formatting

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,14 +1,18 @@
 import itertools
-from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
+
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+
 from .conlleval import conll_evaluation
+
 
 def emotion_detection_metrics_fn(list_hyp, list_label):
     metrics = {}
     metrics["ACC"] = accuracy_score(list_label, list_hyp)
-    metrics["F1"] = f1_score(list_label, list_hyp, average='macro')
-    metrics["REC"] = recall_score(list_label, list_hyp, average='macro')
-    metrics["PRE"] = precision_score(list_label, list_hyp, average='macro')
+    metrics["F1"] = f1_score(list_label, list_hyp, average="macro")
+    metrics["REC"] = recall_score(list_label, list_hyp, average="macro")
+    metrics["PRE"] = precision_score(list_label, list_hyp, average="macro")
     return metrics
+
 
 def aspect_extraction_metrics_fn(list_hyp, list_label):
     metrics = {}
@@ -19,6 +23,7 @@ def aspect_extraction_metrics_fn(list_hyp, list_label):
     metrics["PRE"] = tm_pre
     return metrics
 
+
 def ner_metrics_fn(list_hyp, list_label):
     metrics = {}
     acc, pre, rec, f1, tm_pre, tm_rec, tm_f1 = conll_evaluation(list_hyp, list_label)
@@ -27,6 +32,7 @@ def ner_metrics_fn(list_hyp, list_label):
     metrics["REC"] = tm_rec
     metrics["PRE"] = tm_pre
     return metrics
+
 
 def pos_tag_metrics_fn(list_hyp, list_label):
     metrics = {}
@@ -37,21 +43,24 @@ def pos_tag_metrics_fn(list_hyp, list_label):
     metrics["PRE"] = tm_pre
     return metrics
 
+
 def entailment_metrics_fn(list_hyp, list_label):
     metrics = {}
     metrics["ACC"] = accuracy_score(list_label, list_hyp)
-    metrics["F1"] = f1_score(list_label, list_hyp, average='macro')
-    metrics["REC"] = recall_score(list_label, list_hyp, average='macro')
-    metrics["PRE"] = precision_score(list_label, list_hyp, average='macro')
+    metrics["F1"] = f1_score(list_label, list_hyp, average="macro")
+    metrics["REC"] = recall_score(list_label, list_hyp, average="macro")
+    metrics["PRE"] = precision_score(list_label, list_hyp, average="macro")
     return metrics
+
 
 def document_sentiment_metrics_fn(list_hyp, list_label):
     metrics = {}
     metrics["ACC"] = accuracy_score(list_label, list_hyp)
-    metrics["F1"] = f1_score(list_label, list_hyp, average='macro')
-    metrics["REC"] = recall_score(list_label, list_hyp, average='macro')
-    metrics["PRE"] = precision_score(list_label, list_hyp, average='macro')
+    metrics["F1"] = f1_score(list_label, list_hyp, average="macro")
+    metrics["REC"] = recall_score(list_label, list_hyp, average="macro")
+    metrics["PRE"] = precision_score(list_label, list_hyp, average="macro")
     return metrics
+
 
 def keyword_extraction_metrics_fn(list_hyp, list_label):
     metrics = {}
@@ -62,6 +71,7 @@ def keyword_extraction_metrics_fn(list_hyp, list_label):
     metrics["PRE"] = tm_pre
     return metrics
 
+
 def qa_factoid_metrics_fn(list_hyp, list_label):
     metrics = {}
     acc, pre, rec, f1, tm_pre, tm_rec, tm_f1 = conll_evaluation(list_hyp, list_label)
@@ -71,26 +81,28 @@ def qa_factoid_metrics_fn(list_hyp, list_label):
     metrics["PRE"] = tm_pre
     return metrics
 
+
 def absa_metrics_fn(list_hyp, list_label):
     # hyp and label are both list (multi label), flatten the list
     list_hyp = list(itertools.chain.from_iterable(list_hyp))
     list_label = list(itertools.chain.from_iterable(list_label))
-    
+
     metrics = {}
     metrics["ACC"] = accuracy_score(list_label, list_hyp)
-    metrics["F1"] = f1_score(list_label, list_hyp, average='macro')
-    metrics["REC"] = recall_score(list_label, list_hyp, average='macro')
-    metrics["PRE"] = precision_score(list_label, list_hyp, average='macro')
+    metrics["F1"] = f1_score(list_label, list_hyp, average="macro")
+    metrics["REC"] = recall_score(list_label, list_hyp, average="macro")
+    metrics["PRE"] = precision_score(list_label, list_hyp, average="macro")
     return metrics
+
 
 def news_categorization_metrics_fn(list_hyp, list_label):
     # hyp and label are both list (multi label), flatten the list
     list_hyp = list(itertools.chain.from_iterable(list_hyp))
     list_label = list(itertools.chain.from_iterable(list_label))
-    
+
     metrics = {}
     metrics["ACC"] = accuracy_score(list_label, list_hyp)
-    metrics["F1"] = f1_score(list_label, list_hyp, average='macro')
-    metrics["REC"] = recall_score(list_label, list_hyp, average='macro')
-    metrics["PRE"] = precision_score(list_label, list_hyp, average='macro')
+    metrics["F1"] = f1_score(list_label, list_hyp, average="macro")
+    metrics["REC"] = recall_score(list_label, list_hyp, average="macro")
+    metrics["PRE"] = precision_score(list_label, list_hyp, average="macro")
     return metrics
