a2ac39f7f116853cb330fe88898285b0c1269102
akurniawan <akurniawan.cs@gmail.com>
2021-03-23 09:12:09 +0100

    add parameter to running task

--- run_single_task_meta.sh
+++ run_single_task_meta.sh
@@ -1,8 +1,9 @@
 # mBERT-based model
-MODEL1="mbert-multi-emb-10_embeddings+layer_norm_emb"
-MODEL2="mbert-multi-emb-10_embeddings+layer_norm_emb+layer.0.+layer.1.+layer.2.+layer.3."
-MODEL3="mbert-multi-emb-10_embeddings+layer_norm_emb+layer.0.+layer.1.+layer.2.+layer.3.+layer.4.+layer.5."
-MODEL4="mbert-multi-emb-10_embeddings+layer_norm_emb+layer.0.+layer.1.+layer.2.+layer.3.+layer.4.+layer.5.+layer.6."
+EPOCH="0"
+MODEL1="mbert-multi-emb-10_embeddings+layer_norm_emb/23-02-21/${EPOCH}"
+MODEL2="mbert-multi-emb-10_embeddings+layer_norm_emb+layer.0.+layer.1.+layer.2.+layer.3./26-02-21/${EPOCH}"
+MODEL3="mbert-multi-emb-10_embeddings+layer_norm_emb+layer.0.+layer.1.+layer.2.+layer.3.+layer.4.+layer.5./28-02-21/${EPOCH}"
+MODEL4="mbert-multi-emb-10_embeddings+layer_norm_emb+layer.0.+layer.1.+layer.2.+layer.3.+layer.4.+layer.5.+layer.6./02-03-21/${EPOCH}"
 # embedding
 CUDA_VISIBLE_DEVICES=$1 python3 main.py --n_epochs 1 --train_batch_size $4 --model_checkpoint pretrained/$MODEL1 --step_size 1 --gamma 0.9 --experiment_name $MODEL1_b$4_step1_gamma0.9_lr1e-5_early$3_layer12_lowerFalse --lr 1e-5 --early_stop $3 --dataset $2 --num_layers 12 --force
 # embedding+layer1
